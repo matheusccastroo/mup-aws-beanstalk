1724f3113835497aed895f539ce6b52fa53f2a29
jorgen <jorgen@eri.im>
2024-05-02 16:28:10 +0200

    fix start

--- lib/assets/start.sh
+++ lib/assets/start.sh
@@ -8,14 +8,14 @@ export NVM_DIR="/.nvm"
 [[ ! -z "$MUP_ENV_FILE_VERSION" ]] && { echo "Long Env is enabled."; source /etc/app/env.txt; }
 
 echo "Node version"
-echo $(meteor node --version)
+echo $(node --version)
 echo "Npm version"
-echo $(meteor npm --version)
+echo $(npm --version)
 
 export METEOR_SETTINGS=$(node -e 'console.log(decodeURIComponent(process.env.METEOR_SETTINGS_ENCODED))')
 
 echo "=> Starting health check server"
-meteor node health-check.js &
+node health-check.js &
 
 echo "=> Starting App"
-meteor node main.js
+node main.js
