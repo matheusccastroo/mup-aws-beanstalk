d386f739fd0ecbd776cbd0f696723eed3f62a3fd
jorgen <jorgen@eri.im>
2024-05-02 10:32:21 +0200

    use meteor node

--- src/assets/node.sh
+++ src/assets/node.sh
@@ -2,16 +2,25 @@
 NODE_VERSION=<%= nodeVersion %>
 NPM_VERSION=<%= npmVersion %>
 
+METEOR_VERSION=<%= meteorVersion %>
+
+# We have to install Meteor to use its node and npm versions
+PATH=$PATH:$HOME/.meteor
+cd $HOME
+mkdir -p .meteor/ .npm/ node_modules/
+if [ ! -e $HOME/.meteor/meteor ]; then curl -k "https://install.meteor.com?release=$METEOR_VERSION" | sh; fi
+
 
 export NVM_DIR="/.nvm"
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-nvm alias default $NODE_VERSION
-npm i -g npm@$NPM_VERSION
+# Skip installing node for now, as meteor uses a custom node
+# nvm install $NODE_VERSION
+# nvm use $NODE_VERSION
+# nvm alias default $NODE_VERSION
+# npm i -g npm@$NPM_VERSION
 
 APP_PATH="$(/opt/elasticbeanstalk/bin/get-config container -k app_staging_dir)"
 echo "APP_PATH: $APP_PATH"
@@ -22,4 +31,4 @@ echo "APP_PATH: $APP_PATH"
 
 cd "$APP_PATH"
 ls
-cd programs/server && npm install --unsafe-perm
+cd programs/server && meteor npm install --unsafe-perm
