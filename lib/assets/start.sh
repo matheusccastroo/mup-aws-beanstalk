ff1a22aaf64ee47d1b9c0e14397cec183650ef37
matheusccastro <camargodecastromatheus@gmail.com>
2024-05-11 11:59:09 -0300

    test

--- lib/assets/start.sh
+++ lib/assets/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export NVM_DIR="/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# nvm use default --delete-prefix --silent
-
+echo "Running start.sh script"
 [[ ! -z "$MUP_ENV_FILE_VERSION" ]] && { echo "Long Env is enabled."; source /etc/app/env.txt; }
 
 echo "Node version"
 echo $(node --version)
+echo "Node path"
+echo $(which node)
 echo "Npm version"
 echo $(npm --version)
+echo "Npm path"
+echo $(which npm)
 
 export METEOR_SETTINGS=$(node -e 'console.log(decodeURIComponent(process.env.METEOR_SETTINGS_ENCODED))')
 
